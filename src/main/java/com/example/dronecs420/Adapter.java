384acd0965b2760a36cce35946901a2eac058825
JoshPatton26 <jdp2@uab.edu>
2022-11-17 19:17:28 -0600

    Setting up the adapter class

---
+++ src/main/java/com/example/dronecs420/Adapter.java
@@ -0,0 +1,151 @@
+package com.example.dronecs420;
+
+import java.io.IOException;
+
+public class Adapter extends MultiRotorDrone {
+
+    @Override
+    public void takeoff() throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void land() throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void increaseAltitude(int up) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void decreaseAltitude(int down) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void flyForward(int front) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void flyLeft(int left) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void flyRight(int right) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void turnCW(int degrees) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void turnCCW(int degrees) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getFlightTime() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getHeight() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getAttitudePitch() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getAttitudeRoll() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getAttitudeYaw() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getAccelerationX() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getAccelerationY() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getAccelerationZ() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getTOF() throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void flyBackward(int back) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void flip(String direction) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hoverInPlace(int seconds) throws InterruptedException, IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setSpeed(int speed) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getSpeed() throws NumberFormatException, IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getBattery() throws NumberFormatException, IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
